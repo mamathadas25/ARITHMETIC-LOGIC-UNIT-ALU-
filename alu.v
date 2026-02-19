3ad0aa578f1a860c7bd56e0bf7737d7b60f32718
mamathadas25 <dasmamatha32@gmail.com>
2026-02-19 22:06:30 +0530

    alu.v

--- alu.v
+++ alu.v
@@ -1,17 +1,17 @@
-module simple_alu(
-    input [3:0] A,
-    input [3:0] B,
-    input [1:0] sel,
-    output reg [3:0] result
+module alu (
+    input [3:0] A, B,      // 4-bit input data
+    input [2:0] opcode,    // 3-bit control signal to choose operation
+    output reg [3:0] ALU_Out // 4-bit result
 );
 
-always @(*) begin
-    case(sel)
-        2'b00: result = A + B;
-        2'b01: result = A - B;
-        2'b10: result = A & B;
-        2'b11: result = A | B;
-    endcase
-end
-
+    always @(*) begin
+        case(opcode)
+            3'b000: ALU_Out = A + B;       // Addition
+            3'b001: ALU_Out = A - B;       // Subtraction
+            3'b010: ALU_Out = A & B;       // Logical AND
+            3'b011: ALU_Out = A | B;       // Logical OR
+            3'b100: ALU_Out = ~(A);        // Logical NOT (on input A)
+            default: ALU_Out = 4'b0000;    // Default case
+        endcase
+    end
 endmodule
